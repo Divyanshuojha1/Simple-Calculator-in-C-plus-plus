05d339e2c4ebe6b5bade6f3f40354c0cf5e1512d
Divyanshu Ojha <105076606+Divyanshuojha1@users.noreply.github.com>
2023-07-07 02:39:13 +0530

    Update cal.cpp

--- cal.cpp
+++ cal.cpp
@@ -1,7 +1,7 @@
-[2:27 AM, 7 / 7 / 2023] Dev : #include<iostream>
+#include<iostream>
 #include <string>
 #include <sstream>
-                              using namespace std;
+using namespace std;
 
 float add(float a, float b)
 {
@@ -74,51 +74,5 @@ int main()
     return 0;
 }
 
-This program uses a `stringstream` to parse an arithmetic expression entered by the user, then evaluates each operation in order using functions for addition, subtraction, multiplication, and division. The program includes error handling to prevent division by zero or invalid operators, and uses exceptions to gracefully handle errors and exit the program.
-[2:27 AM, 7/7/2023] Dev: Sure! Here's a simple calculator program in C++ that can perform basic arithmetic operations:
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    float num1, num2, result;
-    char op;
-
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter operator (+, -, *, /): ";
-    cin >> op;
-    cout << "Enter second number: ";
-    cin >> num2;
-
-    switch (op)
-    {
-    case '+':
-        result = num1 + num2;
-        break;
-    case '-':
-        result = num1 - num2;
-        break;
-    case '*':
-        result = num1 * num2;
-        break;
-    case '/':
-        if (num2 != 0)
-        {
-            result = num1 / num2;
-        }
-        else
-        {
-            cout << "Error: division by zero" << endl;
-            return 1;
-        }
-        break;
-    default:
-        cout << "Error: invalid operator" << endl;
-        return 1;
-    }
-
-    cout << "Result: " << result << endl;
-    return 0;
 }
